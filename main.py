e398db2a16cfbfa6bd7c27135977dccdd3d00988
Altro <119718837+Altro31@users.noreply.github.com>
2024-04-23 08:32:35 -0400

    feat: Aplicado varios cambios
    - Se añadió documentación
    - Se cambiaron los nombres de los argumentos de las lambda
    - Se cambió el tamaño y particionado de la lista de prueba

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import dask.bag as db
 from dask.distributed import Client
 
 if __name__ == '__main__':
+    # Crea el nodo máster y los n Workers (n=Cantidad de núcleos de la CPU)
     client = Client()
 
-    lista = db.range(3, 2)
-
-    count = lista.accumulate(lambda x, y: x + y)
-
+    # Crea una lista de números del 0 al 7 y lo particionqa en 4 partes iguales
+    # La lista se guarda en un DaskObject de tipo Bag
+    # Esto pasa pq se usa db (dask.bag) para crear la lista
+    lista = db.range(7, 4)
+    
+    # Se define la función que cada Worker va a aplicar al conjunto de datos que le llegue
+    # En este caso es una función acumuladora, que va iterando valor por valor y va acumulando un valor. Devuelve el valor acumulado.
+    count = lista.accumulate(lambda acc, curr: acc + curr)
+    
+    # Se define la estrategia para recoger los resultados de los Workers
+    # La función fold es muy similar a acumulate
     total_count = count.fold(lambda acc, curr: acc + curr)
 
+    # Para acceder a los datos que contiene un DaskObject se debe usar el método compute()
     print(total_count.compute())
